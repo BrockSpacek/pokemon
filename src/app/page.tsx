da8c00856fbd8350a1504c5fed7c8c271a7d688a
BrockSpacek <brockspacek1@gmail.com>
2025-03-28 15:28:44 -0700

    Local Storage/Favorites List Complete

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,28 +2,19 @@
 import Image from "next/image";
 import BgImage from "@/assets/wallpapersden.com_pokemon-hd-cool-art_1920x1080.jpg";
 import PokeCard from "@/components/PokeCard";
-import {
-  getEvolutionApi,
-  getPokemonApi,
-  LocationApi,
-  PokemonNameApi,
-} from "@/lib/DataServices";
+import { getEvolutionApi, getPokemonApi, LocationApi, PokemonNameApi } from "@/lib/DataServices";
 import { useEffect, useState } from "react";
 import { PokemonInterface } from "@/interface/interface";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
-import {
-  getLocalStorage,
-  saveToLocalStorageByName,
-  removeFromLocalStorage,
-} from "@/lib/DataServices";
+import { getLocalStorage, saveToLocalStorageByName, removeFromLocalStorage } from "@/lib/DataServices";
 
 export default function Home() {
   const [input, setInput] = useState<string | number>("");
   const [searchItem, setSearchItem] = useState<string | number>(1);
   const [pokemonData, setPokemonData] = useState<PokemonInterface | null>(null);
   const [isFavoriteToggled, setIsFavoriteToggled] = useState<boolean>(false);
-  const [favoritesList, setFavoritesList] = useState<string[] | null>(null);
+  const [favoritesList, setFavoritesList] = useState<string[]>([]);
 
   const handleSearchClick = () => {
     if (!input) return;
@@ -49,23 +40,22 @@ export default function Home() {
 
   const toggleFavorites = () => {
     setIsFavoriteToggled(!isFavoriteToggled);
-    console.log(isFavoriteToggled);
+    console.log(favoritesList);
+  };
 
-    getLocalStorage();
-    if(isFavoriteToggled == true){
-      setFavoritesList()
-    }
+  const updateFavoritesList = () => {
+    const list = getLocalStorage();
+    setFavoritesList(list);
   };
 
   useEffect(() => {
+  
     const fetchPokemon = async () => {
       try {
         const pokemonInfo = await PokemonNameApi(searchItem);
         const locationInfo = await LocationApi(pokemonInfo.id);
         const speciesInfo = await getPokemonApi(searchItem);
-        const evolutionInfo = await getEvolutionApi(
-          speciesInfo.evolution_chain.url
-        );
+        const evolutionInfo = await getEvolutionApi(speciesInfo.evolution_chain.url);
 
         const pokemonDetails: PokemonInterface = {
           id: pokemonInfo.id,
@@ -73,21 +63,13 @@ export default function Home() {
           types: pokemonInfo.types.map((type) => type.type.name),
           image: pokemonInfo.sprites.front_default,
           shinyImage: pokemonInfo.sprites.front_shiny,
-          location:
-            locationInfo.length > 0
-              ? locationInfo[0].location_area.name
-              : "N/A",
+          location: locationInfo.length > 0 ? locationInfo[0].location_area.name : "N/A",
           moves: pokemonInfo.moves.map((move) => move.move.name),
-          abilities: pokemonInfo.abilities.map(
-            (ability) => ability.ability.name
-          ),
-          evolutions: evolutionInfo.chain.evolves_to.map(
-            (evo) => evo.species.name
-          ),
+          abilities: pokemonInfo.abilities.map((ability) => ability.ability.name),
+          evolutions: evolutionInfo.chain.evolves_to.map((evo) => evo.species.name),
         };
 
         setPokemonData(pokemonDetails);
-        console.log(pokemonDetails);
       } catch (error) {
         console.error("Error fetching Pokémon data:", error);
       }
@@ -98,16 +80,16 @@ export default function Home() {
     }
   }, [searchItem]);
 
+  useEffect(() => {
+    if (isFavoriteToggled) {
+      updateFavoritesList();
+    }
+  }, [isFavoriteToggled]);
+
   return (
     <div className="w-full">
       <div className="fixed inset-0">
-        <Image
-          src={BgImage}
-          alt="Pokémon Background"
-          fill
-          className="object-cover"
-          priority
-        />
+        <Image src={BgImage} alt="Pokémon Background" fill className="object-cover" priority />
       </div>
 
       <div className="mt-20">
@@ -123,35 +105,38 @@ export default function Home() {
 
         <div className="flex justify-center mt-3 w-full">
           <div className="flex lg:w-[30%] md:w-[50%] w-[75%]">
-            <Button
-              className="relative bg-[#b03535] text-white rounded-[15px] px-4 md:text-lg text-sm flex-1 mx-1 hover:bg-white hover:text-[#b03535]"
-              onClick={handleSearchClick}
-            >
+            <Button className="relative bg-[#b03535] text-white rounded-[15px] px-4 md:text-lg text-sm flex-1 mx-1 hover:bg-white hover:text-[#b03535]" onClick={handleSearchClick}>
               Search
             </Button>
-            <Button
-              className="relative bg-[#b03535] text-white rounded-[15px] px-4 md:text-lg text-sm flex-1 mx-1 hover:bg-white hover:text-[#b03535]"
-              onClick={handleRandomClick}
-            >
+            <Button className="relative bg-[#b03535] text-white rounded-[15px] px-4 md:text-lg text-sm flex-1 mx-1 hover:bg-white hover:text-[#b03535]" onClick={handleRandomClick}>
               Random
             </Button>
-            <Button
-              className="relative bg-[#b03535] text-white rounded-[15px] px-4 md:text-lg text-sm flex-1 mx-1 hover:bg-white hover:text-[#b03535]"
-              onClick={toggleFavorites}
-            >
+            <Button className="relative bg-[#b03535] text-white rounded-[15px] px-4 md:text-lg text-sm flex-1 mx-1 hover:bg-white hover:text-[#b03535]" onClick={toggleFavorites}>
               Favorites
             </Button>
           </div>
         </div>
       </div>
-      <div className={`${isFavoriteToggled ? 'block' : 'hidden'}`}>
-          <div className="relative text-white bg-[#b03535] lg:w-[25%] rounded-2xl text-center ml-14">
-            <h2 className="underline text-3xl">Favorited Pokemon</h2>
-            <p className="lg:text-2xl md:text-lg">{favoritesList}</p>
-          </div>
-        </div>
 
-      <div className="relative w-[87%] p-6 lg:mt-32 mt-24 mx-auto">
+     
+      <div className={`${isFavoriteToggled ? 'block' : 'hidden'}`}>
+  <div className="relative text-white bg-[#b03535] lg:w-[25%] md:w-[40%] w-[50%] rounded-2xl lg:-mt-[95px] mt-5 text-center lg:ml-14 mx-auto py-2">
+    <h2 className="underline lg:text-3xl text-xl">Favorited Pokémon</h2>
+    <div className="lg:text-2xl md:text-lg py-2 overflow-y-auto max-h-30">
+      {favoritesList.length === 0 ? (
+        <p>No favorites added yet.</p>
+      ) : (
+        <ul>
+          {favoritesList.map((name, index) => (
+            <li key={index}>{name}</li>
+          ))}
+        </ul>
+      )}
+    </div>
+  </div>
+</div>
+
+      <div className="relative w-[90%] p-6 lg:mt-32 mt-24 mx-auto">
         {pokemonData && <PokeCard pokemon={pokemonData} />}
       </div>
     </div>
